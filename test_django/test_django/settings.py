5274b9a04395aa8dcc339b46b576ff6d12bfcb1e
vlyagusha <vlyagusha@justclick.ru>
2020-11-09 22:03:28 +0300

    модели и админка джанго

--- test_django/test_django/settings.py
+++ test_django/test_django/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'authors.apps.AuthorsConfig',
     'homepage.apps.HomepageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- test_django/test_django/urls.py
+++ test_django/test_django/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('homepage/', include('homepage.urls')),
+    path('authors/', include('authors.urls')),
 ]
