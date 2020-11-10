13719e211007a9426d6651ae85fe3b0ff97567dd
vlyagusha <vlyagusha@justclick.ru>
2020-11-10 19:23:29 +0300

    модели и админка джанго

--- test_django/authors/views.py
+++ test_django/authors/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+
+from .models import Author
 
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     template_name = 'authors/index.html'
+    model = Author
+    context_object_name = 'authors'
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['authors'] = Author.objects.all()
+    #     return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        print(queryset)
+        queryset = queryset.filter(id__gt=1)
+        print(queryset)
+        return queryset
